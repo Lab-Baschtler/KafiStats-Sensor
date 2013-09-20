3583e3f61c5df02777958b752d224c813ca89444
onmomo <mail@christian-moser.ch>
2013-09-20 10:11:47 +0200

    initialize project

---
+++ coffeCounter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+from time import sleep
+
+from adafruit_libs import Adafruit_TMP006
+
+
+def C_to_F(C):
+    return C * (180.0 / 100.0) + 32.0
+
+# ===========================================================================
+# Example Code
+# ===========================================================================
+
+# Initialise the TMP006 and use defaults
+# tmp = TMP006(0x40, debug=True)
+tmp = Adafruit_TMP006(0x41)
+
+# Start sampling
+tmp.begin()
+
+# Wait a short bit for sample averaging
+while True:
+    print "Pausing 2.0 s..."
+    sleep(2.0)
+
+    dietemp = tmp.readDieTempC()
+    objtemp = tmp.readObjTempC()
+
+    print "Die Temperature:    %.2f C / %.2f F" % (dietemp, C_to_F(dietemp))
+    print "Object Temperature: %.2f C / %.2f F" % (objtemp, C_to_F(objtemp))
+
